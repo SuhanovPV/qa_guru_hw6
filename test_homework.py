846f8c5522960611dd162cabf82f3adac7fd6f3e
pavel.s <ssuxxarr@mail.ru>
2024-10-03 17:24:44 +0300

    solution with ternary conditional operator

--- test_homework.py
+++ test_homework.py
@@ -28,15 +28,18 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    if dark_theme_enabled_by_user is None:
-        if current_time < time(hour=6) or current_time >= time(hour=22):
-            is_dark_theme = True
-        else:
-            is_dark_theme = False
-    elif dark_theme_enabled_by_user:
-        is_dark_theme = True
-    else:
-        is_dark_theme = False
+
+    is_dark_theme = (dark_theme_enabled_by_user, current_time.hour not in range(6, 22))[dark_theme_enabled_by_user is None]
+
+    # if dark_theme_enabled_by_user is None:
+    #     if current_time < time(hour=6) or current_time >= time(hour=22):
+    #         is_dark_theme = True
+    #     else:
+    #         is_dark_theme = False
+    # elif dark_theme_enabled_by_user:
+    #     is_dark_theme = True
+    # else:
+    #     is_dark_theme = False
 
     assert is_dark_theme is True
 
