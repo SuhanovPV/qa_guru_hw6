70ae33ef2ede830c0bf4b353b9f2d1a4ad1fecf4
pavel.s <ssuxxarr@mail.ru>
2024-10-03 11:37:54 +0300

    change condition in tests in test_dark_theme_by_time, test_dark_theme_by_time_and_user_choice. change method of convertion fucnction name

--- test_homework.py
+++ test_homework.py
@@ -7,7 +7,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if current_time < time(hour=6) or current_time > time(hour=22):
+    if current_time < time(hour=6) or current_time >= time(hour=22):
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -27,16 +27,16 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user:
-        is_dark_theme = True
-    else:
-        is_dark_theme = False
 
     if dark_theme_enabled_by_user is None:
-        if current_time < time(hour=6) or current_time > time(hour=22):
+        if current_time < time(hour=6) or current_time >= time(hour=22):
             is_dark_theme = True
         else:
             is_dark_theme = False
+    elif dark_theme_enabled_by_user:
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
 
     assert is_dark_theme is True
 
@@ -80,7 +80,7 @@ def test_find_suitable_user():
 # "Open Browser [Chrome]"
 
 def read_function(func, *args):
-    return f"{' '.join(func.__name__.split('_')).title()} [{', '.join(args)}]"
+    return f"{func.__name__.replace('_', ' ').title()} [{', '.join(args)}]"
 
 
 def test_readable_function():
